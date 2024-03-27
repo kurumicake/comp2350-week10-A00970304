8579dbd9ac5454c4d74ba5cb04b8f339eeeb07d4
kurumicake <73270359+kurumicake@users.noreply.github.com>
2024-03-27 15:02:19 -0700

    fixing connectionsequelize

--- databaseConnectionSequelize.js
+++ databaseConnectionSequelize.js
@@ -1,10 +1,18 @@
-const is_render = process.env.IS_RENDER || false;
-const dbConfigRender = "mysql://freedb_cwan_main:n!?hTMRsJ2GNPM*@ sql.freedb.tech:3306/ freedb_comp2350-week2-A00970304";
+// Convert IS_RENDER environment variable to a boolean correctly
+const isRender = process.env.IS_RENDER === 'true';
+
+// Fixed whitespace issues in the connection string
+const dbConfigRender = "mysql://freedb_cwan_main:n!?hTMRsJ2GNPM*@sql.freedb.tech:3306/freedb_comp2350-week2-A00970304";
+
 const dbConfigLocal = "mysql://root:Coding@626929a44@localhost/lab_example";
-if (is_render) {
-var databaseConnectionString = dbConfigRender;
+
+// Declare databaseConnectionString outside of if-else scope
+let databaseConnectionString;
+
+if (isRender) {
+    databaseConnectionString = dbConfigRender;
+} else {
+    databaseConnectionString = dbConfigLocal;
 }
-else {
-var databaseConnectionString = dbConfigLocal;
-}
+
 module.exports = databaseConnectionString;
