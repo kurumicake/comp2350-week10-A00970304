cc947926ff773d9f49e0afe94215280dd00898b8
kurumicake <73270359+kurumicake@users.noreply.github.com>
2024-03-27 16:31:50 -0700

    testing still

--- databaseConnectionSequelize.js
+++ databaseConnectionSequelize.js
@@ -1,25 +1,25 @@
-// Convert IS_RENDER environment variable to a boolean correctly
-const isRender = process.env.IS_RENDER === 'true';
+// // Convert IS_RENDER environment variable to a boolean correctly
+// const isRender = process.env.IS_RENDER === 'true';
 
-// Fixed whitespace issues in the connection string
-const dbConfigRender = {
-    host: "sql.freedb.tech",
-    user: "freedb_cwan_main",
-    password: "n!?hTMRsJ2GNPM*",
-    database: "freedb_comp2350-week2-A00970304",
-    multipleStatements: false,
-    namedPlaceholders: true
-};
+// // Fixed whitespace issues in the connection string
+// const dbConfigRender = {
+//     host: "sql.freedb.tech",
+//     user: "freedb_cwan_main",
+//     password: "n!?hTMRsJ2GNPM*",
+//     database: "freedb_comp2350-week2-A00970304",
+//     multipleStatements: false,
+//     namedPlaceholders: true
+// };
 
-const dbConfigLocal = "mysql://root:Coding@626929a44@localhost/pets_week10";
+// const dbConfigLocal = "mysql://root:Coding@626929a44@localhost/pets_week10";
 
-// Declare databaseConnectionString outside of if-else scope
-let databaseConnectionString;
+// // Declare databaseConnectionString outside of if-else scope
+// let databaseConnectionString;
 
-if (isRender) {
-    databaseConnectionString = dbConfigRender;
-} else {
-    databaseConnectionString = dbConfigLocal;
-}
+// if (isRender) {
+//     databaseConnectionString = dbConfigRender;
+// } else {
+//     databaseConnectionString = dbConfigLocal;
+// }
 
-module.exports = databaseConnectionString;
+// module.exports = databaseConnectionString;
