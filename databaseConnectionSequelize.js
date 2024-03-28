18d0d089163ddf8736e0c8211003cb350f06f1ed
kurumicake <73270359+kurumicake@users.noreply.github.com>
2024-03-27 17:32:04 -0700

    connection not working still

--- databaseConnectionSequelize.js
+++ databaseConnectionSequelize.js
@@ -1,5 +1,5 @@
 const is_render = process.env.IS_RENDER || false;
-const dbConfigRender = "mysql://freedb_cwan_main:n!?hTMRsJ2GNPM* sql.freedb.tech:3306/ freedb_comp2350-week2-A00970304";
+const dbConfigRender = "mysql://freedb_cwan_main:n!?hTMRsJ2GNPM*@sql.freedb.tech:3306/freedb_comp2350-week2-A00970304";
 const dbConfigLocal = "mysql://root:Password@localhost/pets_week10";
 if (is_render) {
 var databaseConnectionString = dbConfigRender;
