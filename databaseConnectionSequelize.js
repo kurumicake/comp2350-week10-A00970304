baafbd57f0db880a72d8b77e976dd45b8754f347
kurumicake <73270359+kurumicake@users.noreply.github.com>
2024-03-27 18:58:10 -0700

    testing

--- databaseConnectionSequelize.js
+++ databaseConnectionSequelize.js
@@ -1,6 +1,6 @@
 const is_render = process.env.IS_RENDER === 'true'; // Ensures that is_render is a boolean
-const dbConfigRender = "mysql://freedb_cwan_main:n!?hTMRsJ2GNPM*@sql.freedb.tech:3306/freedb_comp2350_week2_A00970304";
-const dbConfigLocal = "mysql://root:Password@localhost/pets_week10";
+const dbConfigRender = "mysql://freedb_cwan_main:n!?hTMRsJ2GNPM*@sql.freedb.tech/freedb_comp2350_week2_A00970304";
+const dbConfigLocal = "mysql://root:Coding@626929a44@localhost/pets_week10";
 
 // Uses ternary operator to set the database connection string based on the environment
 const databaseConnectionString = is_render ? dbConfigRender : dbConfigLocal;
--- routes/router.js
+++ routes/router.js
@@ -6,7 +6,7 @@ const userModel = include('models/web_user');
 const petModel = include('models/pet');
 const petTypeModel = include('models/pet_type');
 const crypto = require('crypto');
-const {v4: uuid} = require('uuid');
+const { v4: uuid } = require('uuid');
 const passwordPepper = "SeCretPeppa4MySal+";
 
 // router.get('/', async (req, res) => {
@@ -28,14 +28,14 @@ const passwordPepper = "SeCretPeppa4MySal+";
 router.get('/', async (req, res) => {
 	console.log("page hit");
 	try {
-		const pets = await petModel.findAll({ attributes: ['pet_id', 'name'] }); 
-		if (pets === null) {
+		const users = await userModel.findAll({ attributes: ['web_user_id', 'first_name', 'last_name', 'email'] }); //{where: {web_user_id: 1}}
+		if (users === null) {
 			res.render('error', { message: 'Error connecting to MySQL' });
 			console.log("Error connecting to userModel");
 		}
 		else {
-			console.log(pets);
-			res.render('index', { allPets: pets });
+			console.log(users);
+			res.render('index', { allUsers: users });
 		}
 	}
 	catch (ex) {
@@ -48,7 +48,7 @@ router.get('/', async (req, res) => {
 router.get('/pets', async (req, res) => {
 	console.log("page hit");
 	try {
-		const pets = await petModel.findAll({ attributes: ['pet_id', 'name'] }); 
+		const pets = await petModel.findAll({ attributes: ['pet_id', 'name'] });
 		if (pets === null) {
 			res.render('error', { message: 'Error connecting to MySQL' });
 			console.log("Error connecting to userModel");
@@ -67,51 +67,75 @@ router.get('/pets', async (req, res) => {
 
 router.get('/deleteUser', async (req, res) => {
 	try {
-	console.log("delete user");
-	let userId = req.query.id;
-	if (userId) {
-	console.log("userId: "+userId);
-	let deleteUser = await userModel.findByPk(userId);
-	console.log("deleteUser: ");
-	console.log(deleteUser);
-	if (deleteUser !== null) {
-	await deleteUser.destroy();
+		console.log("delete user");
+		let userId = req.query.id;
+		if (userId) {
+			console.log("userId: " + userId);
+			let deleteUser = await userModel.findByPk(userId);
+			console.log("deleteUser: ");
+			console.log(deleteUser);
+			if (deleteUser !== null) {
+				await deleteUser.destroy();
+			}
+		}
+		res.redirect("/");
 	}
+	catch (ex) {
+		res.render('error', { message: 'Error connecting to MySQL' });
+		console.log("Error connecting to MySQL");
+		console.log(ex);
 	}
-	res.redirect("/");
-	}
-	catch(ex) {
-	res.render('error', {message: 'Error connecting to MySQL'});
-	console.log("Error connecting to MySQL");
-	console.log(ex);
-	}
-	});
+});
 
-	router.post('/addUser', async (req, res) => {
-		try {
+router.post('/addUser', async (req, res) => {
+	try {
 		console.log("form submit");
 		const password_salt = crypto.createHash('sha512');
 		password_salt.update(uuid());
 		const password_hash = crypto.createHash('sha512');
-		password_hash.update(req.body.password+passwordPepper+password_salt);
+		password_hash.update(req.body.password + passwordPepper + password_salt);
 		let newUser = userModel.build(
-		{
-		first_name: req.body.first_name,
-		last_name: req.body.last_name,
-		email: req.body.email,
-		password_salt: password_salt.digest('hex'),
-		password_hash: password_hash.digest('hex')
-		}
+			{
+				first_name: req.body.first_name,
+				last_name: req.body.last_name,
+				email: req.body.email,
+				password_salt: password_salt.digest('hex'),
+				password_hash: password_hash.digest('hex')
+			}
 		);
 		await newUser.save();
 		res.redirect("/");
-		}
-		catch(ex) {
-		res.render('error', {message: 'Error connecting to MySQL'});
+	}
+	catch (ex) {
+		res.render('error', { message: 'Error connecting to MySQL' });
 		console.log("Error connecting to MySQL");
 		console.log(ex);
+	}
+});
+
+router.get('/showPets', async (req, res) => {
+	console.log("page hit");
+	try {
+		let userId = req.query.id;
+		const user = await userModel.findByPk(userId);
+		if (user === null) {
+			res.render('error', { message: 'Error connecting to MySQL' });
+			console.log("Error connecting to userModel");
 		}
-		});
+		else {
+			let pets = await user.getPets();
+			console.log(pets);
+			let owner = await pets[0].getOwner();
+			console.log(owner);
+			res.render('pets', { allPets: pets });
+		}
+	}
+	catch (ex) {
+		res.render('error', { message: 'Error connecting to MySQL' });
+		console.log("Error connecting to MySQL");
+		console.log(ex);
+	}
+});
 
 module.exports = router;
 
