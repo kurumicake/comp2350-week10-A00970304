a7ef776df45ff54b3f6bef107626e1ed2d81725e
kurumicake <73270359+kurumicake@users.noreply.github.com>
2024-03-27 18:16:08 -0700

    fixing connection

--- databaseConnectionSequelize.js
+++ databaseConnectionSequelize.js
@@ -1,10 +1,8 @@
-const is_render = process.env.IS_RENDER || false;
-const dbConfigRender = "mysql://freedb_cwan_main:n!?hTMRsJ2GNPM*@sql.freedb.tech:3306/freedb_comp2350-week2-A00970304";
+const is_render = process.env.IS_RENDER === 'true'; // Ensures that is_render is a boolean
+const dbConfigRender = "mysql://freedb_cwan_main:n!?hTMRsJ2GNPM*@sql.freedb.tech:3306/freedb_comp2350_week2_A00970304";
 const dbConfigLocal = "mysql://root:Password@localhost/pets_week10";
-if (is_render) {
-var databaseConnectionString = dbConfigRender;
-}
-else {
-var databaseConnectionString = dbConfigLocal;
-}
+
+// Uses ternary operator to set the database connection string based on the environment
+const databaseConnectionString = is_render ? dbConfigRender : dbConfigLocal;
+
 module.exports = databaseConnectionString;
