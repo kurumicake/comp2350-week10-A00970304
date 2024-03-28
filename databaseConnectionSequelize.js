029272957b305ae4e343d3c796cfaf57816f24ae
kurumicake <73270359+kurumicake@users.noreply.github.com>
2024-03-27 19:03:52 -0700

    trying to fix

--- databaseConnectionSequelize.js
+++ databaseConnectionSequelize.js
@@ -1,5 +1,5 @@
 const is_render = process.env.IS_RENDER === 'true'; // Ensures that is_render is a boolean
-"mysql://freedb_comp2350_main:XFZSr3p6c25m@qb@sql.freedb.tech/freedb_comp2350-A01363804";
+const dbConfigRender = "mysql://freedb_comp2350_main:XFZSr3p6c25m@qb@sql.freedb.tech/freedb_comp2350-A01363804";;
 const dbConfigLocal = "mysql://root:Coding@626929a44@localhost/pets_week10";
 
 // Uses ternary operator to set the database connection string based on the environment
