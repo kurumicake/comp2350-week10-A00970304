fb402f22049f1274ccc8d4414441debafaa89f96
kurumicake <73270359+kurumicake@users.noreply.github.com>
2024-03-27 19:18:58 -0700

    updating string

--- models/pet_type.js
+++ models/pet_type.js
@@ -1,6 +1,6 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const databaseConnectionString = include('/databaseConnectionSequelize');
-const sequelize = new Sequelize(databaseConnectionString);
+const sequelize = new Sequelize("mysql://freedb_cwan_main:n!?hTMRsJ2GNPM*@sql.freedb.tech/freedb_comp2350-week2-A00970304");
 
 const PetTypeModel = sequelize.define('pet_type', {
     pet_type_id: {
