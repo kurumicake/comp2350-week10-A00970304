5979e7f6c0d208a7b74c8ed9c2c5eac02b5c13ff
kurumicake <73270359+kurumicake@users.noreply.github.com>
2024-03-27 22:22:30 -0700

    update cred

--- databaseConnectionSequelize.js
+++ databaseConnectionSequelize.js
@@ -1,6 +1,6 @@
 const is_render = process.env.IS_RENDER || false;
-const password = encodeURIComponent("qE%6DmaDpeKvTpj");
-const dbConfigRender = `mysql://freedb_2351_main:${password}@sql.freedb.tech/freedb_comp2350-week2-A01301050`;
+const password = encodeURIComponent("n!?hTMRsJ2GNPM*");
+const dbConfigRender = `mysql://freedb_cwan_main:${password}@sql.freedb.tech/freedb_comp2350-week2-A00970304`;
 const dbConfigLocal = "mysql://root:Coding@626929a44@localhost/pets_week10";
 
 const databaseConnectionString = is_render ? dbConfigRender : dbConfigLocal;
